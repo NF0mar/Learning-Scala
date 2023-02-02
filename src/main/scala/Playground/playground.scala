910c9c16bdaf95b775d198933a02342ca6e3edb9
Nur-farah <nuurfaarax15@gmail.com>
2023-02-02 21:54:50 +0300

    my first projects of scala

---
+++ src/main/scala/Playground/playground.scala
@@ -0,0 +1,9 @@
+package Playground
+
+object playground {
+  def main(args: Array[String]): Unit = {
+    println("I am a true programmer")
+    print("I am also a developer")
+  }
+
+}
---
+++ src/main/scala/lectures/part1basics/ValuesVariablesAndTypes.scala
@@ -0,0 +1,36 @@
+package lectures.part1basics
+
+object ValuesVariablesAndTypes extends App {
+
+//  Using Vals with data types
+  val y: Int = 39
+  println(y)
+
+  //strings
+  val string: String ="I am Nur"
+  val astring = "I am a Developer"
+
+    //char
+  val char: Char = 'N'
+  val achar = 'N'
+
+  //integer
+  val integer: Int = 98
+  val aninteger = 78
+
+  //float and double
+  val float: Float = 87.38f
+  val adouble: Double = 7.91
+
+  //bolean
+  val boolean: Boolean = true
+
+  //short anf long
+  val ashort: Short = 8376
+  val along: Long = 827924762L
+
+//  Using var
+
+  var anint: Int = 8472
+  var magac: String = "Nur"
+}
